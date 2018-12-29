0018b6acd3071e7148417642c65a348b05b43b42
Mxolisi Zondi <mxlzondi@live.com>
2018-12-29 15:50:31 +0200

    Test project code added

--- MyGeotabTests/APITests.cs
+++ MyGeotabTests/APITests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyGeotabClassLibrary;
 
 namespace MyGeotabTests
 {
@@ -7,9 +8,66 @@ namespace MyGeotabTests
     public class APITests
     {
 
-        [TestMethod]
-        public void TestMethod1()
+        MyGeotab myGeotab;
+        string database = "";
+        string password = "";
+        string username = "";
+        string server = "";
+
+        [TestInitialize]
+        public void TestInit()
         {
+            myGeotab = new MyGeotab(new MyCredentials
+            {
+                Password = password,
+                UserName = username,
+                Server = server,
+                Database = database,
+                Authenticate = false
+            });
         }
+
+        [TestMethod]
+        public void TestInitializeWithAuth_ShouldAuthUser()
+        {
+            //Arrange
+
+            //Act
+            var user = myGeotab.GetLoggedInUser();
+            //Assert
+            Assert.AreEqual(username, user.Name.ToLower());
+        }
+
+        [TestMethod]
+        public void TestInitializeWithNoAuth_ShouldAuthUser()
+        {
+            //Arrange
+            myGeotab = new MyGeotab(new MyCredentials
+            {
+                Password = password,
+                UserName = username,
+                Server = server,
+                Database = database,
+                Authenticate = false
+            });
+            //Act
+            var user = myGeotab.GetLoggedInUser();
+            //Assert
+            Assert.AreEqual(username, user.Name.ToLower());
+        }
+
+
+
+        [TestMethod]
+        public void Test_UTCDateConvert_ShouldReturnCurrentDate()
+        {
+            //Arrange
+            DateTime date = DateTime.UtcNow;
+            //Act
+            var result = myGeotab.ToUserDate(date);
+            //Assert
+            Assert.AreEqual(date.ToLocalTime(), result);
+        }
+
     }
 }
